7cfb74bd24f79ee91bd9e92209e9e49640df1777
Ricky Cruz <rczdev@gmail.com>
2020-05-26 18:04:00 -0500

    Add feedback if the answer is correct or not

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -12,7 +12,7 @@
           v-for="(answer, index) in answers"
           :key="index"
           @click.prevent="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : '']"
+          :class="answerClass(index)"
         >
           {{ answer }}
         </b-list-group-item>
@@ -89,6 +89,22 @@ export default {
 
       this.answered = true;
       this.increment(isCorrect);
+    },
+
+    answerClass(index) {
+      let answerClass = '';
+
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = 'selected';
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = 'correct';
+      } else if (this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = 'incorrect'
+      }
+      return answerClass
     }
   }
 }
@@ -110,5 +126,13 @@ export default {
 
   .selected {
     background-color: lightblue;
+  }
+
+  .correct {
+    background-color: lightgreen;
+  }
+
+  .incorrect {
+    background-color: red;
   }
 </style>
